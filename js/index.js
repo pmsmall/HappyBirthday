f701e8b4273a4c6e5154879920be418f61701f2b
RuanJiaFeng <ruanjiafeng2013@163.com>
2021-12-02 11:34:19 +0800

    first commit

--- js/index.js
+++ js/index.js
@@ -10,12 +10,14 @@ $("#login-button").click(function (event) {
   var userName = document.getElementById("userName").value;
   var pwd = document.getElementById("pwd").value;
   //修改密码请改此处
-  if (userName == "123" && pwd == "123") {
+  console.log(userName)
+  console.log(pwd)
+  if (userName == "万老师" && pwd == "1208") {
     event.preventDefault();
     $("form").fadeOut(500);
     $(".wrapper").addClass("form-success");
     setTimeout(function () {
-      location.href = "BirthdayCake.html";
+      location.href = "Memories.html";
     }, 2000);
   } else {
     alert("Wrong Password");
